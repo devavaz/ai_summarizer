d37d8c1a8a5947121cf697d6cfdb246da35b168a
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-01 18:10:18 +0900

    "Hero 'component' to the project and enhance its visual presentation with carefully crafted styles in Tailwind.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {logo} from "../assets"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+   <header className="w-full flex justify-center items-center flex-col">
+   <nav className="flex justify-between items-center w-full mb-10 pt-3">
+      <img src={logo} alt="logo" className="w-28 object-contain" />
+       <button
+       type="button"
+       onClick={() => window.open("https://github.com/devavaz")}
+       className="black_btn">
+        Github
+       </button>
+   </nav>
+
+   <h1 className="head_text">
+    Summarize Articles with <br className="max-md:hidden" /> <span className="orange_gradient">Open AI GPT-4</span>
+   </h1>
+   <h2 className="desc">Simplify your reading with Sumz, open-source article summarizer that transforms lengthy articles into clear and concise summarizes </h2>
+   </header>
   )
 }
 
